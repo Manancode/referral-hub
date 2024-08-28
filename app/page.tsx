90236d18cf279f29654055e9f3a237bc8b0137d4
manancode <mananarorawork@gmail.com>
2024-08-28 16:06:28 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import SignupPage from "./signup/page";
+import SignInPage from "./api/auth/signin/page";
+
 
 export default function Home() {
   return (
-    <div>ui</div>
+    <><SignupPage />
+    <SignInPage /></>
   );
 }
